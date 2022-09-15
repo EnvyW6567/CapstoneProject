7ac0026e5790d9bbcce8c5636969318a06050bec
EnvyW6567 <nbw970508@gmail.com>
2022-09-15 13:58:40 +0900

    jwt login update

--- TeenTam/settings.py
+++ TeenTam/settings.py
@@ -10,7 +10,9 @@ For the fucmdll list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,10 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'corsheaders',
-    'account',
-    'web',
+    'account', # app
+    'web', # app
 ]
 
 
@@ -62,9 +64,46 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication', 
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated', # can access authenticated users
+        'rest_framework.permissions.AllowAny', # can access all users
+        'rest_framework.permissions.IsAdminUser', # cans access only admin
     ),
 }
+
+# Additional JWT Settings
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120), # Token expire time
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7), # Token refresh cycle
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+    
+    # Sliding token setting <Not Using Now>
+    # 'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    # 'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    # 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
 
 
 MIDDLEWARE = [
--- account/views.py
+++ account/views.py
@@ -1,8 +1,12 @@
-from rest_framework.authtoken.models import Token
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import User
+from django.conf import settings
 from django.shortcuts import render 
-from . import token
+from django.contrib.auth.hashers import check_password
+from . import token, authenticate
+import jwt
 
 
 class AccountViewSet(APIView):
@@ -11,3 +15,70 @@ class AccountViewSet(APIView):
         tok = token.CreateToken(user)
         print(tok)
         return render(request, "test.html")
+
+class LoginViewSet(APIView):
+    def post(self, request):
+
+        username = request.GET.get["username"]
+        password = request.GET.get["password"]
+        # blanck username or password
+        if username is None or password is None :
+            return Response({
+                "message" : "username / password required",   
+            }, status = status.HTTP_400_BAD_REQUEST)
+        # not registered username
+        user = User.objects.get(username = username)
+        if user is None:
+            return Response({
+                "message" : "check username",
+            }, status = status.HTTP_404_NOT_FOUND)
+        # wrong password
+        if not check_password(password, user.password):
+            return Response({
+                "message":"wrong password",
+            }, status = status.HTTP_400_BAD_REQUEST)
+
+        response = Response(status=status.HTTP_200_OK)  
+        return authenticate.jwt_login(response, user)
+
+
+
+class RefreshViewSet(APIView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refreshtoken')
+        
+        if refresh_token is None:
+            return Response({
+                "message": "Authentication credentials were not provided."
+            }, status=status.HTTP_403_FORBIDDEN)
+        
+        try:
+            payload = jwt.decode(
+                refresh_token, settings.REFRESH_TOKEN_SECRET, algorithms=['HS256']
+            )
+
+        except:
+            return Response({
+                "message": "expired refresh token, please login again."
+            }, status=status.HTTP_403_FORBIDDEN)
+        
+        user = User.objects.filter(id=payload['user_id']).first()
+        
+        if user is None:
+            return Response({
+                "message": "user not found"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        if not user.is_active:
+            return Response({
+                "message": "user is inactive"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        access_token = generate_access_token(user)
+        
+        return Response(
+            {
+                'access_token': access_token,
+            }
+        )
+        
+        
